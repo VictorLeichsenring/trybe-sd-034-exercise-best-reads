804b3d4972e250fa59bd6c69d5cb3718b9eb6584
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-08-24 02:49:45 -0300

    Criado estado para listas de livros

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Book from './components/Book';
 import BookList from './components/BookList';
 import data from './data.json';
+import { BookListType } from './types';
 
 const bookIndexStart = 10;
 const bookIndexEnd = 15;
@@ -10,6 +11,9 @@ const placeHolderList = data.slice(bookIndexStart, bookIndexEnd); // esse códig
 
 function App() {
   const [indexBook, setIndexBook] = useState(0);
+  const [wishList, setWishList] = useState<BookListType>([]);
+  const [readingList, setReadingList] = useState<BookListType>([]);
+  const [readList, setReadList] = useState<BookListType>([]);
 
   function handleButtonNextBook() {
     if (indexBook + 1 < data.length) {
@@ -19,14 +23,26 @@ function App() {
     }
   }
 
+  function handleWishList() {
+    setWishList([...wishList, data[indexBook]]);
+  }
+
+  function handleReadingList() {
+    setReadingList([...readingList, data[indexBook]]);
+  }
+
+  function handleReadList() {
+    setReadList([...readList, data[indexBook]]);
+  }
+
   return (
     <div className="app">
       <div className="book-selector">
         <Book bookInfo={ data[indexBook] } showDetails />
         <div className="selector-buttons">
-          <button>Adicionar à lista de desejos</button>
-          <button>Adicionar à lista de leitura</button>
-          <button>Adicionar à lista de lidos</button>
+          <button onClick={ handleWishList }>Adicionar à lista de desejos</button>
+          <button onClick={ handleReadingList }>Adicionar à lista de leitura</button>
+          <button onClick={ handleReadList }>Adicionar à lista de lidos</button>
           <button onClick={ handleButtonNextBook }>Próximo livro</button>
         </div>
       </div>
@@ -37,7 +53,7 @@ function App() {
         <button>Exibir lista de lidos</button>
       </div>
       <h1>Lista de ...</h1>
-      <BookList books={ placeHolderList } />
+      <BookList books={ wishList } />
     </div>
   );
 }
